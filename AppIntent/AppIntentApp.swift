44b76702cdc8c3f08a21336a72e8d3d7b568e71e
Aigars Sukurs <aigars@130db.lv>
2023-12-29 04:52:44 +0200

    Initial Commit

---
+++ AppIntent/AppIntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentApp.swift
+//  AppIntent
+//
+//  Created by Aigars Sukurs on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
